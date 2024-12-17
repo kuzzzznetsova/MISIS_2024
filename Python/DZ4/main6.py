8a0651b550c549cee987aba1352df65e3f68ee6f
darina <101723446+kuzzzznetsova@users.noreply.github.com>
2024-12-17 14:18:44 +0300

    Update main6.py

--- Python/DZ4/main6.py
+++ Python/DZ4/main6.py
@@ -1,6 +1,4 @@
-def print_list_elements(elements):
-    for index, value in enumerate(elements):
-        print(f"НОМЕР {index} --> {value}")
-
-my_list = ['я', 'не', 'в', 'отпуск']
-print_list_elements(my_list)
+def print_list(my_list):
+    for index in range(len(my_list)):
+        print(f"НОМЕР {index} --> {my_list[index]}")
+print_list(["я", "не", "в", "отпуск"])
