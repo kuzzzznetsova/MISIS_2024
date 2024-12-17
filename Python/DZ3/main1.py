a1f4f42ecf13d2270b3359d858789e503d64f4ee
darina <101723446+kuzzzznetsova@users.noreply.github.com>
2024-12-17 13:48:50 +0300

    Create main1.py

---
+++ Python/DZ3/main1.py
@@ -0,0 +1,27 @@
+month = input()
+if month == 1:
+  print("Январь")
+elif month == 2:
+  print("Февраль")
+elif month == 3:
+  print("Март")
+elif month == 4:
+  print("Апрель")
+elif month == 5:
+  print("Май")
+elif month == 6:
+  print("Июнь")
+elif month == 7:
+  print("Июль")
+elif month == 8:
+  print("Август")
+elif month == 9:
+  print("Сентябрь")
+elif month == 10:
+  print("Октябрь")
+elif month == 11:
+  print("Ноябрь")
+elif month == 12:
+  print("Декабрь")
+else:
+  print("Некорректный номер месяца")
