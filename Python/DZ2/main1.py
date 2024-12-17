e486497aae9bff85a9ab2ea9df4a3e0e8e4968d1
darina <101723446+kuzzzznetsova@users.noreply.github.com>
2024-12-17 13:36:33 +0300

    Update main1.py

--- Python/DZ2/main1.py
+++ Python/DZ2/main1.py
@@ -1,1 +1,11 @@
+def format_seconds(seconds):
+    days = seconds // 86400
+    hours = (seconds % 86400) // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
 
+    return f"{days}:{hours}:{minutes}:{seconds}"
+
+seconds = 10000
+formatted_time = format_seconds(seconds)
+print(formatted_time)
